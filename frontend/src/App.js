e99dd87caf59cb287a44dc4da5db1b41d23fe0f6
gregtanym <gregtanym@gmail.com>
2024-01-07 05:21:32 +0800

    redesigned navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -73,7 +73,7 @@ export default function App() {
             //setBalance(bal);
             setIsConnected(true);
 
-            navigate('/profile');
+            navigate('/paytoqr');
         }
         catch (error){
             setIsConnected(false);
