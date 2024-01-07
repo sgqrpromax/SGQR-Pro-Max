713872c9d65336acda12c7fbfc1b109a537281ba
gregtanym <gregtanym@gmail.com>
2024-01-07 18:22:45 +0800

    finished the styling of the merchant dashboard page

--- frontend/src/components/merchant/merchant.js
+++ frontend/src/components/merchant/merchant.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Navigate } from "react-router-dom";
 import "./merchant.css";
 import "../../global.css";
@@ -11,11 +11,19 @@ export default function Merchant({ contract, isConnected, web3, address }) {
     const [uenBalance, setUenBalance] = useState('');
     const [transferResult, setTransferResult] = useState('');
 
+    const payeeAddressRef = useRef('')
+    const transferAmountRef = useRef('')
+    const uenBalanceRef = useRef('')
+
     const handleTransfer = async (event) => {
         event.preventDefault();
+        setPayeeAddress(payeeAddressRef.current.value)
+        setTransferAmount(transferAmountRef.current.value)
+        const payeeAddressTmp = payeeAddressRef.current.value
+        const transferAmountTmp = transferAmountRef.current.value
         try {
             // No need to convert amount as Zeenus uses its own denomination
-            const success = await contract.methods.transfer(payeeAddress, transferAmount).send({ from: payeeAddress });
+            const success = await contract.methods.transfer(payeeAddressTmp, transferAmountTmp).send({ from: payeeAddress });
             if (success) {
                 setTransferResult('Transfer successful');
             }
@@ -27,8 +35,10 @@ export default function Merchant({ contract, isConnected, web3, address }) {
 
     const checkUENBalance = async (event) => {
         event.preventDefault();
+        setBalanceUEN(uenBalanceRef.current.value)
+        const balanceUENTmp = uenBalanceRef.current.value
         try {
-            const balance = await contract.methods.balance_of_uen(balanceUEN).call();
+            const balance = await contract.methods.balance_of_uen(balanceUENTmp).call();
             setUenBalance(balance); // Assuming Zeenus uses a denomination that doesn't require conversion
         } catch (error) {
             console.error("Failed to get UEN balance:", error);
@@ -38,47 +48,103 @@ export default function Merchant({ contract, isConnected, web3, address }) {
 
     const MerchantPage = () => {
         return (
+            // <div className="merchant-background">
+            //     <div className="merchant">
+            //         <h2>Merchant Dashboard</h2>
+                    
+            //         {/* Form for transferring Zeenus tokens */}
+            //         <form onSubmit={handleTransfer} className="merchant-form">
+            //             <div className="form-group">
+            //                 <label htmlFor="payeeAddress">Withdraw to Address:</label>
+            //                 <input
+            //                     type="text"
+            //                     id="payeeAddress"
+            //                     value={payeeAddress}
+            //                     onChange={(e) => setPayeeAddress(e.target.value)}
+            //                     placeholder="Enter payee address"
+            //                 />
+            //                 <input
+            //                     type="text"
+            //                     value={transferAmount}
+            //                     onChange={(e) => setTransferAmount(e.target.value)}
+            //                     placeholder="Enter amount to transfer"
+            //                 />
+            //                 <button type="submit">Transfer</button>
+            //             </div>
+            //         </form>
+
+            //         {/* Result of transfer attempt */}
+            //         {transferResult && <p>{transferResult}</p>}
+
+            //         {/* Form for checking UEN balance */}
+            //         <form onSubmit={checkUENBalance} className="merchant-form">
+            //             <div className="form-group">
+            //                 <label htmlFor="uenToCheck">Check Balance for your UEN:</label>
+            //                 <input
+            //                     type="text"
+            //                     id="uenToCheck"
+            //                     value={balanceUEN}
+            //                     onChange={(e) => setBalanceUEN(e.target.value)}
+            //                     placeholder="Enter UEN"
+            //                 />
+            //                 <button type="submit">Check Balance</button>
+            //             </div>
+            //         </form>
+
+            //         {/* Display the balance of a UEN */}
+            //         {uenBalance !== '' && (
+            //             <div className="balance-display">
+            //                 <p>Balance for UEN {balanceUEN}: {uenBalance}</p>
+            //             </div>
+            //         )}
+
+            //     </div>
+
+            //     <GlobalToolBar/>
+            // </div>
             <div className="merchant-background">
                 <div className="merchant">
-                    <h1>Merchant Dashboard</h1>
+                    <h2 style={{margin: '20px'}}>Merchant Dashboard</h2>
                     
                     {/* Form for transferring Zeenus tokens */}
-                    <form onSubmit={handleTransfer} className="merchant-form">
-                        <div className="form-group">
-                            <label htmlFor="payeeAddress">Withdraw to Address:</label>
+                    <form onSubmit={handleTransfer} className="form-group" style={{marginBottom: '40px'}}>
+                        <div style={{fontSize: '30px', marginBottom:'20px'}}>Withdrawal</div>
+                        <div className='withdraw-form-component'>
+                            <div style={{marginRight: "10px"}}>Enter Payee Address: </div>
                             <input
+                                className='input-field'
                                 type="text"
-                                id="payeeAddress"
-                                value={payeeAddress}
-                                onChange={(e) => setPayeeAddress(e.target.value)}
-                                placeholder="Enter payee address"
+                                ref={payeeAddressRef}
+                                placeholder="e.g. 0x12345678"
                             />
-                            <input
-                                type="text"
-                                value={transferAmount}
-                                onChange={(e) => setTransferAmount(e.target.value)}
-                                placeholder="Enter amount to transfer"
-                            />
-                            <button type="submit">Transfer</button>
                         </div>
+                        <div className='withdraw-form-component'>
+                            <div style={{marginRight: "10px"}}>Enter Amount: </div>
+                            <input
+                                className='input-field'
+                                type="text"
+                                ref={transferAmountRef}
+                                placeholder="e.g. 0.25 ZEENUS"
+                            />
+                        </div>
+                        <button type="submit" className='merchant-btn'>Transfer Now</button>
                     </form>
 
                     {/* Result of transfer attempt */}
                     {transferResult && <p>{transferResult}</p>}
 
                     {/* Form for checking UEN balance */}
-                    <form onSubmit={checkUENBalance} className="merchant-form">
-                        <div className="form-group">
-                            <label htmlFor="uenToCheck">Check Balance for your UEN:</label>
+                    <form onSubmit={checkUENBalance} className="form-group">
+                        <div style={{fontSize: '30px', marginBottom:'20px'}}>Check Balance</div>
+                        <div className='withdraw-form-component'>
                             <input
+                                className='input-field'
                                 type="text"
-                                id="uenToCheck"
-                                value={balanceUEN}
-                                onChange={(e) => setBalanceUEN(e.target.value)}
+                                ref={uenBalanceRef}
                                 placeholder="Enter UEN"
                             />
-                            <button type="submit">Check Balance</button>
                         </div>
+                        <button type="submit" className='merchant-btn'>Check Balance</button>
                     </form>
 
                     {/* Display the balance of a UEN */}
@@ -97,7 +163,7 @@ export default function Merchant({ contract, isConnected, web3, address }) {
 
     return (
         <div>
-            {isConnected ? <MerchantPage /> : <Navigate to='/InterfaceDemo' />}
+            {isConnected ? <MerchantPage /> : <Navigate to='/' />}
         </div>
     );
 }
